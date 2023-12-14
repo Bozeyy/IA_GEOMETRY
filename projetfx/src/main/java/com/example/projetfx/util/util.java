bc691e8169d7a0b06c711bfeaac84142c0609254
Gon <gon.fxx@gmail.com>
2023-12-14 11:35:08 +0100

    modif

--- projetfx/src/main/java/com/example/projetfx/util/util.java
+++ projetfx/src/main/java/com/example/projetfx/util/util.java
@@ -1,8 +1,6 @@
 package com.example.projetfx.util;
 
 public class util {
-
-
     public static final double METRE = 40;
     public static final double GRAVITE = METRE*10;
     public static final double VITESSE = METRE*10;
