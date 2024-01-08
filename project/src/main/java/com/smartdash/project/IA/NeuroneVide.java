c7c238d4713963798dde355db4acce608dbd99bb
perez112u <coganperez@gmail.com>
2024-01-08 16:29:00 +0100

    ajout methodes setActive

--- project/src/main/java/com/smartdash/project/IA/NeuroneVide.java
+++ project/src/main/java/com/smartdash/project/IA/NeuroneVide.java
@@ -1,24 +1,18 @@
 package com.smartdash.project.IA;
 
-import com.smartdash.project.modele.objet.Bloc;
 import com.smartdash.project.modele.objet.Objet;
 import com.smartdash.project.modele.objet.Vide;
-
-import java.util.List;
-import java.util.Optional;
 
 public class NeuroneVide extends Neurone{
     public NeuroneVide(int x, int y) {
         super(x, y);
     }
 
-    public boolean isActive(List<Objet> objets) {
-        Optional<Objet> obj = objets.stream().filter(objet -> (objet.getX() == this.x  && objet.getY() == this.y)).findFirst();
-        if (obj.isPresent()) {
-            this.active = obj.get() instanceof Vide;
-        } else {
-            throw new Error("Coordonnées du neuronne non trouvé dans la liste des cases envoyé : (" + this.x + ", " + this.y + ")");
+    @Override
+    public void setActive(int x, int y, String type) {
+        if (this.x == x && this.y == y) {
+            this.active = type.equals("vide");
         }
-        return this.active;
     }
+
 }
