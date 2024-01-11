3f2510f903a9021a22cc7c8f669ffbde6891a728
Grossmann Jeremy <jeremy.grossmann4@etu.univ-lorraine.fr>
2024-01-11 11:48:30 +0100

    Tests des situations sur terrain de 1 à 21 fait

--- project/src/main/tests/com/smartdash/project/modele/TestJoueurSituation.java
+++ project/src/main/tests/com/smartdash/project/modele/TestJoueurSituation.java
@@ -7,6 +7,288 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestJoueurSituation
 {
 
+    @Test
+    public void test_terrain1_saut_reussi_avant_bloc() {
+        Joueur joueur = new Joueur(3, 2, new Terrain("project/src/main/resources/terrains_test_situation/terrain_test1.txt"), new Reseau());
+
+        joueur.sauter();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+
+        assertTrue(joueur.getVivant());
+    }
+
+    @Test
+    public void test_terrain2_saut_reussi_avant_pique() {
+        Joueur joueur = new Joueur(3, 2, new Terrain("project/src/main/resources/terrains_test_situation/terrain_test2.txt"), new Reseau());
+
+        joueur.sauter();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+
+        assertTrue(joueur.getVivant());
+    }
+
+    @Test
+    public void test_terrain2_saut_reussi_avant_avant_pique() {
+        Joueur joueur = new Joueur(2, 2, new Terrain("project/src/main/resources/terrains_test_situation/terrain_test2.txt"), new Reseau());
+
+        joueur.sauter();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+
+        assertTrue(joueur.getVivant());
+    }
+
+    @Test
+    public void test_terrain3_saut_reussi_avant_2_pique() {
+        Joueur joueur = new Joueur(3, 2, new Terrain("project/src/main/resources/terrains_test_situation/terrain_test3.txt"), new Reseau());
+
+        joueur.sauter();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+
+        assertTrue(joueur.getVivant());
+    }
+
+    @Test
+    public void test_terrain3_saut_rate_avant_avant_2_pique() {
+        Joueur joueur = new Joueur(2, 2, new Terrain("project/src/main/resources/terrains_test_situation/terrain_test3.txt"), new Reseau());
+
+        joueur.sauter();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+
+        assertFalse(joueur.getVivant());
+    }
+
+    @Test
+    public void test_terrain4_saut_impossible_avant_3_pique() {
+        Joueur joueur = new Joueur(3, 2, new Terrain("project/src/main/resources/terrains_test_situation/terrain_test4.txt"), new Reseau());
+
+        joueur.sauter();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+
+        assertFalse(joueur.getVivant());
+    }
+
+    @Test
+    public void test_terrain5_atterissage_pique_sol() {
+        Joueur joueur = new Joueur(4, 1, new Terrain("project/src/main/resources/terrains_test_situation/terrain_test5.txt"), new Reseau());
+
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+
+        assertFalse(joueur.getVivant());
+    }
+
+    @Test
+    public void test_terrain5_saut_reussi_pique_sol() {
+        Joueur joueur = new Joueur(4, 1, new Terrain("project/src/main/resources/terrains_test_situation/terrain_test5.txt"), new Reseau());
+
+        joueur.sauter();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+
+        assertTrue(joueur.getVivant());
+    }
+
+    @Test
+    public void test_terrain6_mort_vide_sol() {
+        Joueur joueur = new Joueur(3, 2, new Terrain("project/src/main/resources/terrains_test_situation/terrain_test6.txt"), new Reseau());
+
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+
+        assertFalse(joueur.getVivant());
+    }
+
+    @Test
+    public void test_terrain7_atterissage_2_vide_sol() {
+        Joueur joueur = new Joueur(3, 2, new Terrain("project/src/main/resources/terrains_test_situation/terrain_test7.txt"), new Reseau());
+
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+
+        assertTrue(joueur.getVivant());
+    }
+
+    @Test
+    public void test_terrain8_saut_avant_avant_pique_air() {
+        Joueur joueur = new Joueur(0, 2, new Terrain("project/src/main/resources/terrains_test_situation/terrain_test8.txt"), new Reseau());
+
+        joueur.sauter();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+
+        assertFalse(joueur.getVivant());
+    }
+
+    @Test
+    public void test_terrain8_saut_avant_pique_air() {
+        Joueur joueur = new Joueur(1, 2, new Terrain("project/src/main/resources/terrains_test_situation/terrain_test8.txt"), new Reseau());
+
+        joueur.sauter();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+
+        assertFalse(joueur.getVivant());
+    }
+
+    @Test
+    public void test_terrain8_saut_sous_pique_air() {
+        Joueur joueur = new Joueur(2, 2, new Terrain("project/src/main/resources/terrains_test_situation/terrain_test8.txt"), new Reseau());
+
+        joueur.sauter();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+
+        assertFalse(joueur.getVivant());
+    }
+
+    @Test
+    public void test_terrain8_bis_saut_avant_avant_avant_pique_air() {
+        Joueur joueur = new Joueur(0, 2, new Terrain("project/src/main/resources/terrains_test_situation/terrain_test8_bis.txt"), new Reseau());
+
+        joueur.sauter();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+
+        assertTrue(joueur.getVivant());
+    }
+
+    @Test
+    public void test_terrain8_bis_saut_avant_avant_pique_air() {
+        Joueur joueur = new Joueur(1, 2, new Terrain("project/src/main/resources/terrains_test_situation/terrain_test8_bis.txt"), new Reseau());
+
+        joueur.sauter();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+
+        assertFalse(joueur.getVivant());
+    }
+
+    @Test
+    public void test_terrain8_bis_saut_avant_pique_air() {
+        Joueur joueur = new Joueur(2, 2, new Terrain("project/src/main/resources/terrains_test_situation/terrain_test8_bis.txt"), new Reseau());
+
+        joueur.sauter();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+
+        assertFalse(joueur.getVivant());
+    }
+
+    @Test
+    public void test_terrain8_bis_saut_sous_pique_air() {
+        Joueur joueur = new Joueur(3, 2, new Terrain("project/src/main/resources/terrains_test_situation/terrain_test8_bis.txt"), new Reseau());
+
+        joueur.sauter();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+
+
+        assertTrue(joueur.getVivant());
+    }
+
+    @Test
+    public void test_terrain9_mort_entre_pique1() {
+        Joueur joueur = new Joueur(4, 2, new Terrain("project/src/main/resources/terrains_test_situation/terrain_test9.txt"), new Reseau());
+
+        joueur.sauter();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+
+        assertFalse(joueur.getVivant());
+    }
+
+    @Test
+    public void test_terrain9_mort_entre_pique2() {
+        Joueur joueur = new Joueur(3, 2, new Terrain("project/src/main/resources/terrains_test_situation/terrain_test9.txt"), new Reseau());
+
+        joueur.sauter();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+
+        assertFalse(joueur.getVivant());
+    }
+
+    @Test
+    public void test_terrain9_mort_entre_pique3() {
+        Joueur joueur = new Joueur(2, 2, new Terrain("project/src/main/resources/terrains_test_situation/terrain_test9.txt"), new Reseau());
+
+        joueur.sauter();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+
+        assertFalse(joueur.getVivant());
+    }
+
+    @Test
+    public void test_terrain9_mort_entre_pique4() {
+        Joueur joueur = new Joueur(1, 2, new Terrain("project/src/main/resources/terrains_test_situation/terrain_test9.txt"), new Reseau());
+
+        joueur.sauter();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+
+        assertFalse(joueur.getVivant());
+    }
+
+    @Test
+    public void test_terrain9_saut_reussi() {
+        Joueur joueur = new Joueur(0, 2, new Terrain("project/src/main/resources/terrains_test_situation/terrain_test9.txt"), new Reseau());
+
+        joueur.sauter();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+        joueur.updateJoueur();
+
+
+        assertTrue(joueur.getVivant());
+    }
+
+
+
+
+
 
 
     @Test
