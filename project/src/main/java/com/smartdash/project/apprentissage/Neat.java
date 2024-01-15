dba0096d62356d14b37985b623e1cc73922ab31e
Gon <gon.fxx@gmail.com>
2024-01-15 14:55:50 +0100

    add: -ajout squelette de code de base de l'apprentissage

---
+++ project/src/main/java/com/smartdash/project/apprentissage/Neat.java
@@ -0,0 +1,39 @@
+package com.smartdash.project.apprentissage;
+
+import com.smartdash.project.IA.Reseau;
+
+import java.util.List;
+
+public class Neat {
+
+    public void lancerApprentissage()
+    {
+        // TODO
+    }
+
+    public int evaluerPerformance(Reseau reseau)
+    {
+        // TODO
+        return 0;
+    }
+
+    public Reseau croisement(Reseau parent1, Reseau parent2)
+    {
+        // TODO
+        return null;
+    }
+
+    public Reseau mutation(Reseau reseau)
+    {
+        // TODO
+
+        return null;
+    }
+
+    public List<Reseau> selectionnerParents(List<Reseau> population)
+    {
+        // TODO
+
+        return null;
+    }
+}
